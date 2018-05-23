8c065d4eae850dc7207aa781a85e8f0faa286bb2
Jason Sturges <jason@jasonsturges.com>
2018-05-22 22:00:25 -0500

    Scaffold console app

---
+++ SqliteConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SqliteConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
