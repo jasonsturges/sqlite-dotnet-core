9c81f5c62b749aa34cb0e17a0051870298af109e
Jason Sturges <jason@jasonsturges.com>
2022-12-29 23:10:13 -0600

    Convert to top-level statements

--- SqliteConsole/Program.cs
+++ SqliteConsole/Program.cs
@@ -5,46 +5,37 @@ using Microsoft.Extensions.Logging;
 using SqliteConsole.Infrastructure.Data;
 using SqliteConsole.Infrastructure.Services;
 
-namespace SqliteConsole
-{
-    class Program
+// Configuration
+var builder = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+var configuration = builder.Build();
+
+// Database
+var optionsBuilder = new DbContextOptionsBuilder<SqliteConsoleContext>()
+    .UseSqlite(configuration.GetConnectionString("DefaultConnection"));
+var context = new SqliteConsoleContext(optionsBuilder.Options);
+context.Database.EnsureCreated();
+
+// Services
+var services = new ServiceCollection()
+    .AddLogging(loggingBuilder =>
     {
-        static void Main(string[] args)
-        {
-            // Configuration
-            var builder = new ConfigurationBuilder()
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
-            var configuration = builder.Build();
+        loggingBuilder.AddConsole();
+    })
+    .AddSingleton(configuration)
+    .AddSingleton(optionsBuilder.Options)
+    .AddSingleton<IExampleService, ExampleService>()
+    .AddDbContextPool<SqliteConsoleContext>(options => options.UseSqlite(configuration.GetConnectionString("DefaultConnection")))
+    .BuildServiceProvider();
 
-            // Database
-            var optionsBuilder = new DbContextOptionsBuilder<SqliteConsoleContext>()
-                .UseSqlite(configuration.GetConnectionString("DefaultConnection"));
-            var context = new SqliteConsoleContext(optionsBuilder.Options);
-            context.Database.EnsureCreated();
+var logger = (services.GetService<ILoggerFactory>() ?? throw new InvalidOperationException())
+    .CreateLogger<Program>();
 
-            // Services
-            var services = new ServiceCollection()
-                .AddLogging(loggingBuilder =>
-                {
-                    loggingBuilder.AddConsole();
-                })
-                .AddSingleton(configuration)
-                .AddSingleton(optionsBuilder.Options)
-                .AddSingleton<IExampleService, ExampleService>()
-                .AddDbContextPool<SqliteConsoleContext>(options => options.UseSqlite(configuration.GetConnectionString("DefaultConnection")))
-                .BuildServiceProvider();
+logger.LogInformation($"Starting application at: {DateTime.Now}");
 
-            var logger = (services.GetService<ILoggerFactory>() ?? throw new InvalidOperationException())
-                .CreateLogger<Program>();
-
-            logger.LogInformation($"Starting application at: {DateTime.Now}");
-
-            // Example Service
-            var service = services.GetService<IExampleService>();
-            service?.AddExample("Test A");
-            service?.AddExample("Test B");
-            service?.AddExample("Test C");
-            service?.GetExamples();
-        }
-    }
-}
+// Example Service
+var service = services.GetService<IExampleService>();
+service?.AddExample("Test A");
+service?.AddExample("Test B");
+service?.AddExample("Test C");
+service?.GetExamples();
