d267c158d2e6764e5fd5b41cf6718637d1cedba1
Jason Sturges <jason@jasonsturges.com>
2022-12-29 22:25:29 -0600

    Entity example

---
+++ SqliteConsole.Core/Entities/Example.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqliteConsole.Core.Entities
+{
+    public class Example
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+    }
+}
