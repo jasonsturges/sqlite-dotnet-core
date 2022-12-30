f6866986b29d9f4cc558717150208ad1c0b44f20
Jason Sturges <jason@jasonsturges.com>
2022-12-29 22:26:25 -0600

    Sqlite database context options builder

--- SqliteConsole/Program.cs
+++ SqliteConsole/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using SqliteConsole.Infrastructure.Data;
+
+namespace SqliteConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Configuration
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+            var configuration = builder.Build();
+
+            // Database
+            var optionsBuilder = new DbContextOptionsBuilder<SqliteConsoleContext>()
+                .UseSqlite(configuration.GetConnectionString("DefaultConnection"));
+            var context = new SqliteConsoleContext(optionsBuilder.Options);
+            context.Database.EnsureCreated();
+        }
+    }
+}
